fb13a745f9056d4d716cab068616ed4faa38d3e3
CristianV350 <cristian.vicol@tarainteractive.com>
2023-03-16 23:12:24 +0200

    add bonjour api

--- src/services/shared.ts
+++ src/services/shared.ts
@@ -3,9 +3,8 @@ export let shared = {
     VUE_APP_DEBUG: '',
     VUE_APP_ISSUER: '',
     VUE_APP_CLIENT_ID: '',
-    VUE_APP_API_SCHEME: 'http',
-    VUE_APP_API_HOST: 'localhost',
-    VUE_APP_API_PORT: '3080',
+    VUE_APP_API_SCHEME: 'https',
+    VUE_APP_API_HOST: 'bonjourapi.herokuapp.com',
     VUE_APP_API_SUBPATH: '/api',
   },
 }
